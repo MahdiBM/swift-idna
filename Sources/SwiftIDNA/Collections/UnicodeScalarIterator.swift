4408deea029e0ab91cbb13a504364454f8132f87
MahdiBM <github@mahdibm.com>
2026-07-20 21:05:28 +0330

    Branchless scalar decoding + fix miscalc which skipped a short-circuit

--- Sources/SwiftIDNA/Collections/UnicodeScalarIterator.swift
+++ Sources/SwiftIDNA/Collections/UnicodeScalarIterator.swift
@@ -18,27 +18,26 @@ struct UnicodeScalarIterator {
     @inline(__always)
     @inlinable
     mutating func uncheckedNext(in bytes: Span<UInt8>) -> Unicode.Scalar {
-        let firstByte = unsafe bytes[unchecked: self.currentCodeUnitOffset]
+        let lowerBound = self.currentCodeUnitOffset
+        let firstByte = unsafe bytes[unchecked: lowerBound]
 
         /// Count of leading ones in the first byte: 0 for ASCII, else the scalar's byte-length
         let leadingOnes = Swift.min(4, (~firstByte).leadingZeroBitCount)
         let scalarUTF8Length = Swift.max(1, leadingOnes)
+        self.currentCodeUnitOffset = lowerBound &+ scalarUTF8Length
 
-        let lowerBound = self.currentCodeUnitOffset
-        self.currentCodeUnitOffset &+= scalarUTF8Length
+        let lastIndex = bytes.count &- 1
+        let byte1 = unsafe bytes[unchecked: Swift.min(lowerBound &+ 1, lastIndex)]
+        let byte2 = unsafe bytes[unchecked: Swift.min(lowerBound &+ 2, lastIndex)]
+        let byte3 = unsafe bytes[unchecked: Swift.min(lowerBound &+ 3, lastIndex)]
 
         var value = UInt32(firstByte & (0b0111_1111 &>> leadingOnes))
+        value = (value &<< 6) | UInt32(byte1 & 0b0011_1111)
+        value = (value &<< 6) | UInt32(byte2 & 0b0011_1111)
+        value = (value &<< 6) | UInt32(byte3 & 0b0011_1111)
+        value = value &>> (6 &* (4 &- scalarUTF8Length))
 
-        let iterRange = unsafe Range<Int>(
-            uncheckedBounds: (lowerBound &+ 1, self.currentCodeUnitOffset)
-        )
-        for byteIdx in iterRange {
-            let byte = unsafe bytes[unchecked: byteIdx]
-            value = (value &<< 6) | UInt32(byte & 0b0011_1111)
-        }
-        let scalar = unsafe Unicode.Scalar(value).unsafelyUnwrapped
-
-        return scalar
+        return unsafe Unicode.Scalar(value).unsafelyUnwrapped
     }
 
     /// Decodes and returns the next Unicode scalar.
