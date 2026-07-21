0843818150b28785dd7928f402bf60def771f890
MahdiBM <github@mahdibm.com>
2026-07-21 22:21:31 +0330

    Add random-order benchs

--- Benchmarks/ToASCII/ToASCII.swift
+++ Benchmarks/ToASCII/ToASCII.swift
@@ -351,6 +351,103 @@ let benchmarks: @Sendable () -> Void = {
         blackHole(domainName)
     }
 
+    /// Mark: - Multiple_Domains data
+    /// Grabbed from Cloudflare top domains
+
+    // [
+    //     "лодочный-причал.рф",
+    //     "мойассистент.рф",
+    //     "мвд.рф",
+    //     "高速下载.com",
+    //     "全球下载.com",
+    //     "赛格gps.公司",
+    //     "šijanec.eu",
+    //     "lepší.tv",
+    //     "نسوانجي.net",
+    //     "ångströ.com",
+    //     "求人ボックス.com",
+    //     "т.website",
+    //     "szukamksiążki.pl",
+    // ]
+    let multipleDomains: [13 of [UInt8]] = [
+        [
+            0xD0, 0xBB, 0xD0, 0xBE, 0xD0, 0xB4, 0xD0, 0xBE,
+            0xD1, 0x87, 0xD0, 0xBD, 0xD1, 0x8B, 0xD0, 0xB9,
+            0x2D, 0xD0, 0xBF, 0xD1, 0x80, 0xD0, 0xB8, 0xD1,
+            0x87, 0xD0, 0xB0, 0xD0, 0xBB, 0x2E, 0xD1, 0x80,
+            0xD1, 0x84,
+        ],
+        [
+            0xD0, 0xBC, 0xD0, 0xBE, 0xD0, 0xB9, 0xD0, 0xB0,
+            0xD1, 0x81, 0xD1, 0x81, 0xD0, 0xB8, 0xD1, 0x81,
+            0xD1, 0x82, 0xD0, 0xB5, 0xD0, 0xBD, 0xD1, 0x82,
+            0x2E, 0xD1, 0x80, 0xD1, 0x84,
+        ],
+        [
+            0xD0, 0xBC, 0xD0, 0xB2, 0xD0, 0xB4, 0x2E, 0xD1,
+            0x80, 0xD1, 0x84,
+        ],
+        [
+            0xE9, 0xAB, 0x98, 0xE9, 0x80, 0x9F, 0xE4, 0xB8,
+            0x8B, 0xE8, 0xBD, 0xBD, 0x2E, 0x63, 0x6F, 0x6D,
+        ],
+        [
+            0xE5, 0x85, 0xA8, 0xE7, 0x90, 0x83, 0xE4, 0xB8,
+            0x8B, 0xE8, 0xBD, 0xBD, 0x2E, 0x63, 0x6F, 0x6D,
+        ],
+        [
+            0xE8, 0xB5, 0x9B, 0xE6, 0xA0, 0xBC, 0x67, 0x70,
+            0x73, 0x2E, 0xE5, 0x85, 0xAC, 0xE5, 0x8F, 0xB8,
+        ],
+        [
+            0xC5, 0xA1, 0x69, 0x6A, 0x61, 0x6E, 0x65, 0x63,
+            0x2E, 0x65, 0x75,
+        ],
+        [
+            0x6C, 0x65, 0x70, 0xC5, 0xA1, 0xC3, 0xAD, 0x2E,
+            0x74, 0x76,
+        ],
+        [
+            0xD9, 0x86, 0xD8, 0xB3, 0xD9, 0x88, 0xD8, 0xA7,
+            0xD9, 0x86, 0xD8, 0xAC, 0xD9, 0x8A, 0x2E, 0x6E,
+            0x65, 0x74,
+        ],
+        [
+            0xC3, 0xA5, 0x6E, 0x67, 0x73, 0x74, 0x72, 0xC3,
+            0xB6, 0x2E, 0x63, 0x6F, 0x6D,
+        ],
+        [
+            0xE6, 0xB1, 0x82, 0xE4, 0xBA, 0xBA, 0xE3, 0x83,
+            0x9C, 0xE3, 0x83, 0x83, 0xE3, 0x82, 0xAF, 0xE3,
+            0x82, 0xB9, 0x2E, 0x63, 0x6F, 0x6D,
+        ],
+        [
+            0xD1, 0x82, 0x2E, 0x77, 0x65, 0x62, 0x73, 0x69,
+            0x74, 0x65,
+        ],
+        [
+            0x73, 0x7A, 0x75, 0x6B, 0x61, 0x6D, 0x6B, 0x73,
+            0x69, 0xC4, 0x85, 0xC5, 0xBC, 0x6B, 0x69, 0x2E,
+            0x70, 0x6C,
+        ],
+    ]
+
+    let multipleDomainsICU: [13 of String] = [
+        "лодочный-причал.рф",
+        "мойассистент.рф",
+        "мвд.рф",
+        "高速下载.com",
+        "全球下载.com",
+        "赛格gps.公司",
+        "šijanec.eu",
+        "lepší.tv",
+        "نسوانجي.net",
+        "ångströ.com",
+        "求人ボックス.com",
+        "т.website",
+        "szukamksiążki.pl",
+    ]
+
     for (namePrefix, idnaConfig) in nameAndConfigs {
         /// Mark: - öob.se
         /// Grabbed from Cloudflare top 1M domains
@@ -523,6 +620,107 @@ let benchmarks: @Sendable () -> Void = {
                 var domainName = "生命之花.中国"
                 domainName = UIDNAHookICU.encode(domainName)!
                 blackHole(domainName)
+            }
+        }
+
+        /// Mark: - Multiple_Domains
+
+        Benchmark(
+            "To_ASCII_\(namePrefix)_Multiple_Domains_CPU_200K",
+            configuration: .init(
+                metrics: [.cpuUser],
+                warmupIterations: 15,
+                maxIterations: 1000,
+            )
+        ) { benchmark in
+            var rng = FastRNG()
+            for _ in 0..<200_000 {
+                let idx = Int(rng.next() % UInt64(multipleDomains.count))
+                let result = try! idnaConfig.toASCII(
+                    _uncheckedAssumingValidUTF8: multipleDomains[idx].span
+                )
+                let domainName = unsafe result.collect().unsafelyUnwrapped
+                blackHole(domainName)
+            }
+        }
+
+        Benchmark(
+            "To_ASCII_\(namePrefix)_Multiple_Domains_Malloc",
+            configuration: .init(
+                metrics: [.mallocCountTotal],
+                warmupIterations: 1,
+                maxIterations: 10,
+            )
+        ) { benchmark in
+            for idx in multipleDomains.indices {
+                let result = try! idnaConfig.toASCII(
+                    _uncheckedAssumingValidUTF8: multipleDomains[idx].span
+                )
+                let domainName = unsafe result.collect().unsafelyUnwrapped
+                blackHole(domainName)
+            }
+        }
+
+        Benchmark(
+            "To_ASCII_\(namePrefix)_Multiple_Domains_Instructions",
+            configuration: .init(
+                metrics: [.instructions],
+                warmupIterations: 10,
+                maxIterations: 10,
+            )
+        ) { benchmark in
+            for idx in multipleDomains.indices {
+                let result = try! idnaConfig.toASCII(
+                    _uncheckedAssumingValidUTF8: multipleDomains[idx].span
+                )
+                let domainName = unsafe result.collect().unsafelyUnwrapped
+                blackHole(domainName)
+            }
+        }
+
+        if namePrefix.lowercased() == "lax" {
+            Benchmark(
+                "To_ASCII_\(namePrefix)_Multiple_Domains_CPU_200K_ICU",
+                configuration: .init(
+                    metrics: [.cpuUser],
+                    warmupIterations: 15,
+                    maxIterations: 1000,
+                )
+            ) { benchmark in
+                var rng = FastRNG()
+                for _ in 0..<200_000 {
+                    let idx = Int(rng.next() % UInt64(multipleDomainsICU.count))
+                    let domainName = UIDNAHookICU.encode(multipleDomainsICU[idx])!
+                    blackHole(domainName)
+                }
+            }
+
+            Benchmark(
+                "To_ASCII_\(namePrefix)_Multiple_Domains_Malloc_ICU",
+                configuration: .init(
+                    metrics: [.mallocCountTotal],
+                    warmupIterations: 1,
+                    maxIterations: 10,
+                )
+            ) { benchmark in
+                for idx in multipleDomainsICU.indices {
+                    let domainName = UIDNAHookICU.encode(multipleDomainsICU[idx])!
+                    blackHole(domainName)
+                }
+            }
+
+            Benchmark(
+                "To_ASCII_\(namePrefix)_Multiple_Domains_Instructions_ICU",
+                configuration: .init(
+                    metrics: [.instructions],
+                    warmupIterations: 10,
+                    maxIterations: 10,
+                )
+            ) { benchmark in
+                for idx in multipleDomainsICU.indices {
+                    let domainName = UIDNAHookICU.encode(multipleDomainsICU[idx])!
+                    blackHole(domainName)
+                }
             }
         }
     }
--- Benchmarks/ToUnicode/ToUnicode.swift
+++ Benchmarks/ToUnicode/ToUnicode.swift
@@ -351,6 +351,113 @@ let benchmarks: @Sendable () -> Void = {
         blackHole(domainName)
     }
 
+    /// Mark: - Multiple_Domains data
+    /// Grabbed from Cloudflare top domains
+
+    // [
+    //     "xn----8sbkrdkflhbom0gg8c.xn--p1ai",
+    //     "xn--80akicokc0aablc.xn--p1ai",
+    //     "xn--b1aew.xn--p1ai",
+    //     "xn--ghq880n3na965a.com",
+    //     "xn--ghqu5fm27b67w.com",
+    //     "xn--gps-8y0gm25n.xn--55qx5d",
+    //     "xn--ijanec-9jb.eu",
+    //     "xn--lep-tma39c.tv",
+    //     "xn--mgbkt9eckr.net",
+    //     "xn--ngstr-lra8j.com",
+    //     "xn--pckua2a7gp15o89zb.com",
+    //     "xn--r1a.website",
+    //     "xn--szukamksiki-4kb16m.pl",
+    // ]
+    let multipleDomains: [13 of [UInt8]] = [
+        [
+            0x78, 0x6E, 0x2D, 0x2D, 0x2D, 0x2D, 0x38, 0x73,
+            0x62, 0x6B, 0x72, 0x64, 0x6B, 0x66, 0x6C, 0x68,
+            0x62, 0x6F, 0x6D, 0x30, 0x67, 0x67, 0x38, 0x63,
+            0x2E, 0x78, 0x6E, 0x2D, 0x2D, 0x70, 0x31, 0x61,
+            0x69,
+        ],
+        [
+            0x78, 0x6E, 0x2D, 0x2D, 0x38, 0x30, 0x61, 0x6B,
+            0x69, 0x63, 0x6F, 0x6B, 0x63, 0x30, 0x61, 0x61,
+            0x62, 0x6C, 0x63, 0x2E, 0x78, 0x6E, 0x2D, 0x2D,
+            0x70, 0x31, 0x61, 0x69,
+        ],
+        [
+            0x78, 0x6E, 0x2D, 0x2D, 0x62, 0x31, 0x61, 0x65,
+            0x77, 0x2E, 0x78, 0x6E, 0x2D, 0x2D, 0x70, 0x31,
+            0x61, 0x69,
+        ],
+        [
+            0x78, 0x6E, 0x2D, 0x2D, 0x67, 0x68, 0x71, 0x38,
+            0x38, 0x30, 0x6E, 0x33, 0x6E, 0x61, 0x39, 0x36,
+            0x35, 0x61, 0x2E, 0x63, 0x6F, 0x6D,
+        ],
+        [
+            0x78, 0x6E, 0x2D, 0x2D, 0x67, 0x68, 0x71, 0x75,
+            0x35, 0x66, 0x6D, 0x32, 0x37, 0x62, 0x36, 0x37,
+            0x77, 0x2E, 0x63, 0x6F, 0x6D,
+        ],
+        [
+            0x78, 0x6E, 0x2D, 0x2D, 0x67, 0x70, 0x73, 0x2D,
+            0x38, 0x79, 0x30, 0x67, 0x6D, 0x32, 0x35, 0x6E,
+            0x2E, 0x78, 0x6E, 0x2D, 0x2D, 0x35, 0x35, 0x71,
+            0x78, 0x35, 0x64,
+        ],
+        [
+            0x78, 0x6E, 0x2D, 0x2D, 0x69, 0x6A, 0x61, 0x6E,
+            0x65, 0x63, 0x2D, 0x39, 0x6A, 0x62, 0x2E, 0x65,
+            0x75,
+        ],
+        [
+            0x78, 0x6E, 0x2D, 0x2D, 0x6C, 0x65, 0x70, 0x2D,
+            0x74, 0x6D, 0x61, 0x33, 0x39, 0x63, 0x2E, 0x74,
+            0x76,
+        ],
+        [
+            0x78, 0x6E, 0x2D, 0x2D, 0x6D, 0x67, 0x62, 0x6B,
+            0x74, 0x39, 0x65, 0x63, 0x6B, 0x72, 0x2E, 0x6E,
+            0x65, 0x74,
+        ],
+        [
+            0x78, 0x6E, 0x2D, 0x2D, 0x6E, 0x67, 0x73, 0x74,
+            0x72, 0x2D, 0x6C, 0x72, 0x61, 0x38, 0x6A, 0x2E,
+            0x63, 0x6F, 0x6D,
+        ],
+        [
+            0x78, 0x6E, 0x2D, 0x2D, 0x70, 0x63, 0x6B, 0x75,
+            0x61, 0x32, 0x61, 0x37, 0x67, 0x70, 0x31, 0x35,
+            0x6F, 0x38, 0x39, 0x7A, 0x62, 0x2E, 0x63, 0x6F,
+            0x6D,
+        ],
+        [
+            0x78, 0x6E, 0x2D, 0x2D, 0x72, 0x31, 0x61, 0x2E,
+            0x77, 0x65, 0x62, 0x73, 0x69, 0x74, 0x65,
+        ],
+        [
+            0x78, 0x6E, 0x2D, 0x2D, 0x73, 0x7A, 0x75, 0x6B,
+            0x61, 0x6D, 0x6B, 0x73, 0x69, 0x6B, 0x69, 0x2D,
+            0x34, 0x6B, 0x62, 0x31, 0x36, 0x6D, 0x2E, 0x70,
+            0x6C,
+        ],
+    ]
+
+    let multipleDomainsICU: [13 of String] = [
+        "xn----8sbkrdkflhbom0gg8c.xn--p1ai",
+        "xn--80akicokc0aablc.xn--p1ai",
+        "xn--b1aew.xn--p1ai",
+        "xn--ghq880n3na965a.com",
+        "xn--ghqu5fm27b67w.com",
+        "xn--gps-8y0gm25n.xn--55qx5d",
+        "xn--ijanec-9jb.eu",
+        "xn--lep-tma39c.tv",
+        "xn--mgbkt9eckr.net",
+        "xn--ngstr-lra8j.com",
+        "xn--pckua2a7gp15o89zb.com",
+        "xn--r1a.website",
+        "xn--szukamksiki-4kb16m.pl",
+    ]
+
     for (namePrefix, idnaConfig) in nameAndConfigs {
         /// Mark: - öob.se
         /// Grabbed from Cloudflare top 1M domains
@@ -523,6 +630,107 @@ let benchmarks: @Sendable () -> Void = {
                 var domainName = "xn--9iqv4mb85adml.xn--fiqs8s"
                 domainName = UIDNAHookICU.decode(domainName)!
                 blackHole(domainName)
+            }
+        }
+
+        /// Mark: - Multiple_Domains
+
+        Benchmark(
+            "To_Unicode_\(namePrefix)_Multiple_Domains_CPU_200K",
+            configuration: .init(
+                metrics: [.cpuUser],
+                warmupIterations: 15,
+                maxIterations: 1000,
+            )
+        ) { benchmark in
+            var rng = FastRNG()
+            for _ in 0..<200_000 {
+                let idx = Int(rng.next() % UInt64(multipleDomains.count))
+                let result = try! idnaConfig.toUnicode(
+                    _uncheckedAssumingValidUTF8: multipleDomains[idx].span
+                )
+                let domainName = unsafe result.collect().unsafelyUnwrapped
+                blackHole(domainName)
+            }
+        }
+
+        Benchmark(
+            "To_Unicode_\(namePrefix)_Multiple_Domains_Malloc",
+            configuration: .init(
+                metrics: [.mallocCountTotal],
+                warmupIterations: 1,
+                maxIterations: 10,
+            )
+        ) { benchmark in
+            for idx in multipleDomains.indices {
+                let result = try! idnaConfig.toUnicode(
+                    _uncheckedAssumingValidUTF8: multipleDomains[idx].span
+                )
+                let domainName = unsafe result.collect().unsafelyUnwrapped
+                blackHole(domainName)
+            }
+        }
+
+        Benchmark(
+            "To_Unicode_\(namePrefix)_Multiple_Domains_Instructions",
+            configuration: .init(
+                metrics: [.instructions],
+                warmupIterations: 10,
+                maxIterations: 10,
+            )
+        ) { benchmark in
+            for idx in multipleDomains.indices {
+                let result = try! idnaConfig.toUnicode(
+                    _uncheckedAssumingValidUTF8: multipleDomains[idx].span
+                )
+                let domainName = unsafe result.collect().unsafelyUnwrapped
+                blackHole(domainName)
+            }
+        }
+
+        if namePrefix.lowercased() == "lax" {
+            Benchmark(
+                "To_Unicode_\(namePrefix)_Multiple_Domains_CPU_200K_ICU",
+                configuration: .init(
+                    metrics: [.cpuUser],
+                    warmupIterations: 15,
+                    maxIterations: 1000,
+                )
+            ) { benchmark in
+                var rng = FastRNG()
+                for _ in 0..<200_000 {
+                    let idx = Int(rng.next() % UInt64(multipleDomainsICU.count))
+                    let domainName = UIDNAHookICU.decode(multipleDomainsICU[idx])!
+                    blackHole(domainName)
+                }
+            }
+
+            Benchmark(
+                "To_Unicode_\(namePrefix)_Multiple_Domains_Malloc_ICU",
+                configuration: .init(
+                    metrics: [.mallocCountTotal],
+                    warmupIterations: 1,
+                    maxIterations: 10,
+                )
+            ) { benchmark in
+                for idx in multipleDomainsICU.indices {
+                    let domainName = UIDNAHookICU.decode(multipleDomainsICU[idx])!
+                    blackHole(domainName)
+                }
+            }
+
+            Benchmark(
+                "To_Unicode_\(namePrefix)_Multiple_Domains_Instructions_ICU",
+                configuration: .init(
+                    metrics: [.instructions],
+                    warmupIterations: 10,
+                    maxIterations: 10,
+                )
+            ) { benchmark in
+                for idx in multipleDomainsICU.indices {
+                    let domainName = UIDNAHookICU.decode(multipleDomainsICU[idx])!
+                    blackHole(domainName)
+                }
             }
         }
     }
